a750899909efdbf85b2345c5eee357499d55dc52
Wang <377638695@qq.com>
2025-09-23 20:30:25 +0800

    lab3

--- lab3/randomizedtest/BuggyAList.java
+++ lab3/randomizedtest/BuggyAList.java
@@ -63,7 +63,7 @@ public class BuggyAList<Item> {
     public Item removeLast() {
         if(size==0) return null;
         if ((size < items.length / 4) && (size > 4)) {
-            resize(size / 4);
+            resize(items.length / 4);
         }
         Item x = getLast();
         items[size - 1] = null;
